9e48935dce55490d573785e052d891020d7be709
doodlesun <76955830+doodlesun@users.noreply.github.com>
2022-02-06 14:55:15 +0100

    add about, privacy, custom navbar

---
+++ main.js
@@ -0,0 +1,29 @@
+window.onload = (event) => {
+    let darkModeSwitch = document.getElementById('dark-mode-toggle');
+    let darkModeStorage = localStorage.getItem('dark-mode');
+    if (darkModeStorage === 'false') {
+        setLightMode();
+    } else {
+        setDarkMode()
+    }
+    darkModeSwitch.addEventListener('click', () => {
+        let currentColor = document.documentElement.style.getPropertyValue('--main-color');
+        if (currentColor === '#000000') {
+            setDarkMode();
+            localStorage.setItem('dark-mode', true.toString());
+        } else {
+            setLightMode();
+            localStorage.setItem('dark-mode', false.toString());
+        }
+    })
+    function setDarkMode() {
+        document.documentElement.style.setProperty('--main-color', '#FFFFFF');
+        document.documentElement.style.setProperty('--background-color', '#000000');
+        darkModeSwitch.textContent = 'Light Mode';
+    }
+    function setLightMode() {
+        document.documentElement.style.setProperty('--main-color', '#000000');
+        document.documentElement.style.setProperty('--background-color', '#FFFFFF');
+        darkModeSwitch.textContent = 'Dark Mode';
+    }
+}
